0851671b3f763b618d8dc7971921eb6d537c5c9b
demonicthorns <kemal.gulumser@hotmail.com>
2022-11-07 23:25:22 +0300

    update.

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { slotFlagsText } from "@vue/shared";
+import { ref, onMounted } from "vue";
+const transcript = ref("");
+const isRecording = ref(false);
+
+const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const sr = new Recognition();
+
+onMounted(() => {
+  sr.continuous = true;
+  sr.interimResults = true;
+
+  sr.onStart = () => {
+    console.log("SR started");
+    isRecording.value = true;
+  };
+
+  sr.onend = () => {
+    console.log("SR Stopped");
+    isRecording.value = false;
+  };
+
+  sr.onresult = (evt) => {
+    for (let i = 0; i < evt.results.lenght; i++) {
+      const result = evt.results[i];
+
+      if (result.isFinal) CheckForCommand(result);
+    }
+
+    const t = Array.from(evt.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+
+    transcript.value = t;
+  };
+});
+
+const CheckForCommand = (result) => {
+  if (result[0].transcript.incluıdes("stop recoring")) {
+    sr.stop();
+  } else if (t.includes("what is the time") || t.includes("what's the time")) {
+    sr.stop();
+    alert(new Date().toLocaleTimeString());
+    setTimeout(() => sr.start(), 100);
+  }
+};
+
+const ToggleMic = () => {
+  if (isRecording.value) {
+    sr.stop();
+  } else {
+    sr.start();
+  }
+};
+</script>
+
+<template>
+  <div class="app">
+    <button :class="mic" @click="ToggleMic">Record</button>
+    <div class="transcript" v-text="transcript"></div>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Courier New", Courier, monospace;
+}
+
+body {
+  background-color: blueviolet;
+  color: aliceblue;
+}
+</style>
